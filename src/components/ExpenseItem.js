5b8509ed9c5e0ffe785fb85798ed7007c58946f9
Shwach <puuupo@gmail.com>
2023-05-21 13:15:40 +0900

    컴포넌트 분리

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,29 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
-function ExpenseItem() 
-{
+function ExpenseItem(props) {
+    //리액트와 상관없는 순수 자바스크립트 (동적 표현을 위한 상수 선언)
+    //{}안에 변수명을 넣는다
+    //Date형식은 .toISOString()를 뒤에 붙여야 한다.
+
+    //props : 컴포넌트를 재사용하기 위한 
+    //App.js에 정의된 배열을 출력
+    
+    /*const expenseDate = new Date(2021,2, 28); 
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67;*/
+
+   
+
     return (
         <div className="expense-item">
-            <div>March 28th 2021</div>
+            {/*<div>{props.date.toISOString()}</div>*/}
+
+            <ExpenseDate date={props.date}></ExpenseDate>
+
             <div className = "expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className = "expense-item__price">$294.37</div>
+                <h2>{props.title}</h2> 
+                <div className = "expense-item__price">${props.amount}</div>
             </div>
         </div>
     );
